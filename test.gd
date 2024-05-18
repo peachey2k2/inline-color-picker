ee79cc106bf83a42c5dd13fec1d2be5c8a8d6810
peachey2k2 <ogzpolat@gmail.com>
2024-05-18 23:20:43 +0300

    first attempt

---
+++ test.gd
@@ -0,0 +1,25 @@
+@tool
+extends Node
+
+@export var trigger:bool:
+	set(val):
+		#f(EditorInterface.get_script_editor().get_current_editor().get_base_editor(),0)
+		f(EditorInterface.get_script_editor().get_current_editor().get_base_editor(),0)
+
+
+func f(node:Node, level:int):
+
+	print("- ".repeat(level), node.name, " (", node.get_class(), ")")
+
+	for child in node.get_children(true):
+		f(child, level+1)
+		Color(1,1,1,1)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
